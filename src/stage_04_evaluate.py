149485cfd909fdfa33ff3cd0d1788b44710aaf9a
Gaurav <gauravsaini.728@gmail.com>
2021-10-11 22:23:11 +0530

    stage_04 evaluate added

---
+++ src/stage_04_evaluate.py
@@ -0,0 +1,68 @@
+from src.utils.all_utils import read_yaml, create_directory, save_data, evaluate_accuracy, save_reports
+import os
+import json
+import argparse
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+from sklearn.metrics import accuracy_score
+
+
+def evaluate_score():
+    pass
+
+def evaluate(config_path, params_path):
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    # access local directory and saved artifacts
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+
+    # access split data directory
+    split_data_dir = config["artifacts"]["split_data_dir"]
+    split_data_dir_path = os.path.join(artifacts_dir, split_data_dir)
+
+    # access testing data
+    test_data_filename = config["artifacts"]["test"]
+    test_data_path = os.path.join(split_data_dir_path, test_data_filename)
+
+    # access features and lables
+    test_data = pd.read_csv(test_data_path)
+    y_test = test_data["class"]
+    x_test = test_data.drop("class", axis=1)
+
+    # accessing model directory and saved model
+    model_dir = config["artifacts"]["model_dir"]
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    model_filename = config["artifacts"]["model_filename"]
+    model_file_path = os.path.join(model_dir_path, model_filename)
+
+    # creating scores directory and saving the report in json format
+    scores_dir = config["artifacts"]["scores_dir"]
+    scores_dir_path = os.path.join(artifacts_dir, scores_dir)
+    create_directory([scores_dir_path])
+
+    scores_filename = config["artifacts"]["scores_filename"]
+    scores_file_path = os.path.join(scores_dir_path, scores_filename)
+
+    RFClassifier = joblib.load(model_file_path)
+
+    predicted_values = RFClassifier.predict(x_test.values)
+    accuracy = evaluate_accuracy(y_test.values, predicted_values)
+    # print(accuracy)
+
+    scores = {
+        "accuracy": accuracy,
+    }
+
+    save_reports(report=scores, report_path=scores_file_path)
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    
+    args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+
+    parsed_args = args.parse_args()
+
+    evaluate(config_path=parsed_args.config, params_path=parsed_args.params)
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import csv
 import json
+from sklearn.metrics import accuracy_score
 
 
 def read_yaml(path_to_yaml: str) -> dict:
@@ -17,4 +18,13 @@ def create_directory(dirs: list):
 def save_data(data, data_path, index_status=False):
     data.to_csv(data_path, index=index_status)
     print(f"data saved at {data_path}")
+
+def evaluate_accuracy(actual_values, predicted_values):
+    accuracy = accuracy_score(actual_values, predicted_values)
+    return accuracy
+
+def save_reports(report: dict, report_path: str, indentation=4):                   
+    with open(report_path, "w") as f:
+        json.dump(report, f, indent=indentation)
+    print(f"reports are saved at {report_path}")  
 
